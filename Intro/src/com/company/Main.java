46f05892f91759dcd925e477c17f2e43a585374d
Yahya Hussein <yahyahussein91@gmail.com>
2021-02-10 16:30:56 -0800

    added more variables

--- Intro/src/com/company/Main.java
+++ Intro/src/com/company/Main.java
@@ -7,7 +7,12 @@ public class Main {
         System.out.println("Hello Yahya");
 
         //Declare a Variable
-        int myFirstNumnber = 5;
+        int myFirstNumnber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 40;
+        int myThirdNumber = 3000;
+        int myTotal = myFirstNumnber + mySecondNumber + mySecondNumber;
+        //Print out value of myFirstNumber®
+        System.out.println(myTotal);
 
     }
 }
