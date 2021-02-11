e96845404c4c16065984c66441da5469a181c66c
Yahya Hussein <yahyahussein91@gmail.com>
2021-02-10 19:30:58 -0800

    done with conversion challenge

--- Intro/src/com/company/Main.java
+++ Intro/src/com/company/Main.java
@@ -60,6 +60,27 @@ public class Main {
         System.out.println(newLong);
 
         //Floating point numbers
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " + myMaxFloatValue);
 
+        //Double values
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " + myMaxDoubleValue);
+
+        int myIntValue = 5 / 2;
+        float myFloatValue = 5f / 2f;
+        double myDoubleValue = 5d / 2d;
+        System.out.println("MyIntValue = " + myIntValue);
+        System.out.println("MyFloatValue = " + myFloatValue);
+        System.out.println("MyDoubleValue = " + myDoubleValue);
+
+        //Conversion Challenge
+        double pounds = 120;
+        double conversion = pounds * 0.45359237;
+        System.out.println(conversion);
     }
 }
