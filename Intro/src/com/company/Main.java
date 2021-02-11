9976efdaecf0b4372bdeef41637852f30862c6fa
Yahya Hussein <yahyahussein91@gmail.com>
2021-02-10 20:04:21 -0800

    done with basic operators

--- Intro/src/com/company/Main.java
+++ Intro/src/com/company/Main.java
@@ -117,5 +117,20 @@ public class Main {
 
         //Operators
         int result = 1 + 2;
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+        result = result - 1;
+        System.out.println("3 - 1 = " + result);
+
+        result = result * 10;
+        System.out.println("2 * 10 = " + result);
+
+        result = result / 5;
+        System.out.println("20 / 5 = " + result);
+
+        result = result % 3;
+        System.out.println("4 % 3 = " + result);
+
     }
 }
