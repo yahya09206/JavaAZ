ee7f72424a8a9ff77b4e9391be4ac5bfa879fe59
Yahya Hussein <yahyahussein91@gmail.com>
2021-02-10 16:43:15 -0800

    playing around with min max values

--- Intro/src/com/company/Main.java
+++ Intro/src/com/company/Main.java
@@ -7,14 +7,24 @@ public class Main {
         System.out.println("Hello Yahya");
 
         //Declare a Variable
-        int myFirstNumnber = (10 + 5) + (2 * 10);
+        int myFirstNumber = (10 + 5) + (2 * 10);
         int mySecondNumber = 40;
-        int myThirdNumber = myFirstNumnber * 2;
-        int myTotal = myFirstNumnber + mySecondNumber + myThirdNumber;
-        int myLastOne = myTotal - 1000;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+        int myLastOne = 1000 - myTotal;
         //Print out value of myFirstNumber®
         System.out.println(myTotal);
         System.out.println(myLastOne);
+
+        //Int Byte Short
+        int myValue = 10000;
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        System.out.println("Busted Min value = " + (myMinIntValue - 1));
 
     }
 }
